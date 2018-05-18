f3886a0c35cfc9d1d132343c2da2d3796aec845a
Jessica Skok <jessica.skok@gmail.com>
2018-05-17 20:00:01 -0700

    css formatting randomizing answers

--- alarm.js
+++ alarm.js
@@ -1,31 +1,40 @@
 //let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 let alarmSound = new Audio();
-alarmSound.src = 'alarm.mp3';
+alarmSound.src = 'schno.mp3';
 let alarmTimer;
 let modal = document.querySelector(".modal");
 let request = new XMLHttpRequest();
 const app = document.getElementById('myModalContent');
 const container = document.createElement('div');
+const buttonContainer = document.createElement('div');
+buttonContainer.setAttribute('class', 'container');
 container.setAttribute('class', 'container');
 app.appendChild(container);
+container.appendChild(buttonContainer);
 
 
-request.open('GET', 'https://opentdb.com/api.php?amount=10&category=11&difficulty=easy', true);
 
-request.onload = function (){
-  let data = JSON.parse(this.response);
-  let myData = data.results;
- if (request.status >= 200 && request.status < 400) {
-  myData.forEach(result => {
+const requestFunction = function(){
+
+  request.open('GET', 'https://opentdb.com/api.php?amount=1&category=18&difficulty=easy&type=multiple', true);
+
+  request.onload = function (){
+    let data = JSON.parse(this.response);
+    console.log(data);
+    let myData = data.results;
+    if (request.status >= 200 && request.status < 400) {
+    myData.forEach(result => {
     //console.log(result.question);
     const card = document.createElement('div');
     card.setAttribute('class', 'card');
 
     const question = document.createElement('h2');
     question.textContent = result.question;
+    console.log(question.textContent);
 
     const correctButton = document.createElement('button');
     const correctText = document.createElement('p');
+
     correctText.textContent = result.correct_answer;
     correctButton.appendChild(correctText);
 
@@ -46,24 +55,38 @@ request.onload = function (){
       incorrectButton3Text.textContent = result.incorrect_answers[2];
       incorrectButton3.appendChild(incorrectButton3Text);
     }
+  buttonContainer.appendChild(correctButton);
+  buttonContainer.appendChild(incorrectButton1);
+  buttonContainer.appendChild(incorrectButton2);
+  buttonContainer.appendChild(incorrectButton3);
 
+  for (let i = buttonContainer.children.length; i >=0; i--){
+    buttonContainer.appendChild(buttonContainer.children[Math.random() * i | 0]);
+  }
   container.appendChild(card);
 
   card.appendChild(question);
-  card.appendChild(correctButton);
-  card.appendChild(incorrectButton1);
-  card.appendChild(incorrectButton2);
-  card.appendChild(incorrectButton3);
+  card.appendChild(buttonContainer);
+ 
 
+
+
+  correctButton.setAttribute('onclick', 'popupFunction(this)');
+    
   });
   } else {
     const errorMessage = document.createElement('marquee');
     errorMessage.textContent = `SOMETHING'S WRONG!`;
     app.appendChild(errorMessage);
   }
-  }
 
-request.send();
+  }
+  request.send();
+}
+
+
+console.log(requestFunction());
+
 
 function popupFunction(){
   modal.classList.toggle("show-modal");
@@ -80,10 +103,10 @@ function setAlarm(button){
     let alarmTime = new Date(alarm.getUTCFullYear(), alarm.getUTCMonth(), alarm.getUTCDate(),  alarm.getUTCHours(), alarm.getUTCMinutes(), alarm.getUTCSeconds());
     let differenceInMs = alarmTime.getTime() - (new Date()).getTime();
 
-    if (differenceInMs < 0) {
-      alert('Set an alarm for the future!');
-      return;
-    }
+    // if (differenceInMs < 0) {
+    //   alert('Set an alarm for the future!');
+    //   return;
+    // }
 
     alarmTimer = setTimeout(initAlarm, differenceInMs, popupFunction);
 
